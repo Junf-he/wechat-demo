0b4971d9a317438d41dafcb8a3438c9a7187f178
bi <2214871523@qq.com>
2019-11-19 19:36:24 +0800

    首页按钮及回到当前位置按钮功能

--- ofo微信小程序demo/pages/index/index.js
+++ ofo微信小程序demo/pages/index/index.js
@@ -9,7 +9,13 @@ Page({
     longitude:"121.47",
     controls:[]
   },
- 
+  bindcontroltap: function (e) {
+    switch (e.controlId) {
+      case 1:
+        this.movetoCenter();
+        break;
+    }
+  },
   /**
    * 生命周期函数--监听页面加载
    */
@@ -20,6 +26,68 @@ Page({
         this.setData({
           latitude:res.latitude,
           longitude:res.longitude
+        })
+      },
+    })
+    wx.getSystemInfo({
+      success: (res) => {
+        this.setData({
+          controls: [
+            {
+              id: 1,
+              iconPath: "/image/ic_location.png",
+              position: {
+                width: 30,
+                height: 30,
+                left: 50,
+                top: res.windowHeight - 120
+              },
+              clickable: true
+            },
+            {
+              id: 2,
+              iconPath: "/image/timg.jpg",
+              position: {
+                width: 100,
+                height: 100,
+                left: res.windowWidth / 2 - 50,
+                top: res.windowHeight - 200
+              },
+              clickable: true
+            },
+            {
+              id: 3,
+              iconPath: "/image/journey.png",
+              position: {
+                width: 100,
+                height: 100,
+                left: 280,
+                top: res.windowHeight - 160
+              },
+              clickable: true
+            },
+            {
+              id: 4,
+              iconPath: "/image/bicycle1.png",
+              position: {
+                width: 30,
+                height: 30,
+                left: 250,
+                top: res.windowHeight - 300
+              },
+            },
+            {
+              id: 5,
+              iconPath: "/image/mine.png",
+              position: {
+                width: 100,
+                height: 100,
+                left: 250,
+                top: res.windowHeight - 300
+              },
+              clickable: true
+            }
+          ]
         })
       },
     })
